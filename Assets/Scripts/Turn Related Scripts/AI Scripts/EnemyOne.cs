9f083cc0ab5484dbf7cfb136fd406d5c784496cf
Shemamforash <samwest2@hotmail.co.uk>
2013-12-03 11:38:00 +0000

    Commit 19
    Improved players, now doesnt work at all. Functions more general.

---
+++ Assets/Scripts/Turn Related Scripts/AI Scripts/EnemyOne.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyOne : AIBasicParent
+{
+	void Awake()
+	{
+		turnInfoScript = gameObject.GetComponent<TurnInfo>();
+		enemyOneTurnScript = gameObject.GetComponent<EnemyOne>();
+	}
+	
+	public void SetRace()
+	{
+		Debug.Log(playerTurnScript.playerRace);
+
+		if(playerTurnScript.playerRace == "Humans" || playerTurnScript.playerRace == "Selkies")
+		{
+			NereidesStart ();
+		}
+		if(playerTurnScript.playerRace == "Nereides")
+		{
+			HumansStart ();
+		}
+	}
+	
+	void NereidesStart()
+	{
+		playerRace = "Nereides";
+		materialInUse = nereidesMaterial;
+		nereidesHomeSystem = homeSystem;
+		RaceStart (playerRace, nereidesHomeSystem, materialInUse);
+	}
+	
+	void HumansStart()
+	{
+		playerRace = "Humans";
+		materialInUse = humansMaterial;
+		humansHomeSystem = homeSystem;
+		RaceStart (playerRace, humansHomeSystem, materialInUse);
+	}
+	
+	void RaceStart(string thisRace, string thisHome, Material thisMaterial)
+	{
+		Debug.Log (thisRace + thisHome);
+
+		PickRace();
+		
+		turnInfoScript.systemsInPlay++;
+		
+		GP = raceGP;
+		
+		lineRenderScript = GameObject.Find(thisHome).GetComponent<LineRenderScript>();
+		lineRenderScript.ownedBy = thisRace;
+		
+		StartSystemPlanetColonise(thisMaterial, thisHome, ownedSystems);
+	}
+}
+
