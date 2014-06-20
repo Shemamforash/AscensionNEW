2a356d34c1d88d63d58a06bd38a139130c56c0c9
Shemamforash <samwest2@hotmail.co.uk>
2014-06-20 16:57:20 +0100

    Commit 1.20
    More UI work.

---
+++ Assets/Scripts/UIScripts/TokenUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TokenUI : MasterScript 
+{
+	public Vector3 originalPosition;
+	public GameObject originalParent;
+
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
