db4db9979230b6a8a027c59f578a0ec059250ae2
Shemamforash <samwest2@hotmail.co.uk>
2014-06-25 14:31:35 +0100

    Revert "Commit 1.23"
    This reverts commit 14ec0a2224960c94efc91710295074a8f7f027d3.

--- Assets/Scripts/UIScripts/Invasion/TokenUI.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TokenUI : MasterScript 
-{
-	public Vector3 originalPosition;
-	public GameObject originalParent;
-	public GameObject hero;
-}
